76fe15b77de7657ae8ae956a8466d7bbc10e50b3
tarum <block-code@mail.ru>
2024-03-26 11:55:46 +0300

    change auth middleware

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -5,31 +5,43 @@ namespace App\Middleware;
 use App\Application\Middleware\Middleware;
 use App\Application\Config\Config;
 use App\Application\Response\Response;
+use App\Models\User;
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 use Firebase\JWT\SignatureInvalidException;
 
 
-class AuthMiddleware extends Middleware {
-    
-    public function handle() {
+class AuthMiddleware extends Middleware
+{
+
+    public function handle()
+    {
         $auth_token = getallheaders()['Authorization'] ?? false;
-        
-        if(!$auth_token){
+
+        if (!$auth_token) {
             $response = json_encode(['error' => 'unauthorized']);
             http_response_code(403);
             header('content-type: application/json');
             echo $response;
             exit;
         }
-        
+
         $secret_key = Config::get('auth.jwt_secret');
-        
+
         $auth_token = explode(' ', $auth_token)[1];
 
         try {
-            $decoded = JWT::decode($auth_token, new Key($secret_key, 'HS256'));
+            $decoded = (array) JWT::decode($auth_token, new Key($secret_key, 'HS256'));
+
+            if (!$user = (new User())->find('token', $secret_key)) {
+                $response = json_encode(['error' => 'unauthorized']);
+                http_response_code(403);
+                header('content-type: application/json');
+                echo $response;
+                exit;
+            }
+
         } catch (SignatureInvalidException $th) {
             $response = json_encode(['error' => 'unauthorized']);
             http_response_code(403);
